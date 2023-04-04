7b1b358ce690b777dff847645aad64b6dacbf877
BaptisteAg <45164638+BaptisteAg@users.noreply.github.com>
2023-04-04 09:49:01 +0200

    init

---
+++ index.js
@@ -0,0 +1,68 @@
+const Readable = require('stream').Readable;
+const ftpClient = require("basic-ftp");
+
+module.exports = {
+    init(config) {
+        const getConnection = async () => {
+            const client = new ftpClient.Client();
+            await client.access({
+                host: config.host,
+                user: config.user,
+                password: config.password,
+                secure: config.secure,
+            });
+            return client;
+        };
+
+        const uploadStream = async (file) => {
+            const path = `${config.path}/${file.hash}${file.ext}`;
+            const client = await getConnection();
+
+            try {
+                const source = new Readable();
+                source._read = () => {}; // _read is required but you can noop it
+                source.push(file.buffer);
+                source.push(null);
+                await client.uploadFrom(source, path);
+
+            } catch (error) {
+                throw error;
+            } finally {
+                await client.close();
+            }
+        };
+
+        const deleteFile = async (file) => {
+            const path = `${config.path}/${file.hash}${file.ext}`;
+            const client = await getConnection();
+
+            try {
+                await client.remove(path);
+            } catch (error) {
+                throw error;
+            } finally {
+                await client.close();
+            }
+        };
+
+        return {
+            async upload(file) {
+                await uploadStream(file);
+                file.url = `/uploads/${file.hash}${file.ext}`;
+            },
+
+
+            delete(file) {
+                return new Promise((resolve, reject) => {
+                    deleteFile(file)
+                        .then(() => {
+                               resolve();
+                        })
+                        .catch((error) => {
+                            reject(error);
+                        });
+                });
+            },
+        };
+    },
+};
